99a22ddff3c5bf97a183ec0194f957f1bc2cde96
Jeremy Johnson <jeremy.m.johnson@davenportiowa.com>
2022-09-19 14:49:33 -0500

    improved Set-ProfileScript error handling

--- scripts/Set-ProfileScript.ps1
+++ scripts/Set-ProfileScript.ps1
@@ -1,9 +1,16 @@
-$value = ". $PSScriptRoot\Set-UbuntuConfig.ps1"
-$path = $PROFILE.CurrentUserAllHosts
+function Set-ProfileScript {
+    $Local:ErrorActionPreference = 'SilentlyContinue'
+    $value = ". $PSScriptRoot\Set-UbuntuConfig.ps1"
+    $path = $PROFILE.CurrentUserAllHosts
 
-$valueExists = Select-String -Path "$path" -Pattern "$value" -SimpleMatch
+    $valueExists = Select-String -Path "$path" -Pattern "$value" -SimpleMatch -Quiet
 
-if (-not $valueExists) {
-    Add-Content -Path "$path" -Value "`n$value"
-    . "$($PROFILE.CurrentUserAllHosts)" # reload the profile for the current session
+    if (-not $valueExists) {
+        Add-Content -Path "$path" -Value "`n$value"
+    }
 }
+
+Set-ProfileScript
+
+# reload the profile for the current session
+. "$($PROFILE.CurrentUserAllHosts)"
