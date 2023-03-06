7a94652ea528da6944dc6d72401b7b5057d05ad1
Jeremy Johnson <modernclassicgamer@gmail.com>
2023-03-05 22:29:56 -0600

    update kubectl-aliases.sh

--- roles/shell/files/kubectl-aliases.sh
+++ roles/shell/files/kubectl-aliases.sh
@@ -1,4 +1,4 @@
 # Ansible managed
 
-KUBECTL_ALIASES="$HOME/source/repos/kubectl-aliases/.kubectl_aliases"
+KUBECTL_ALIASES="$HOME/source/repos/jeremyj563/kubectl-aliases/.kubectl_aliases"
 [ -f $KUBECTL_ALIASES ] && source $KUBECTL_ALIASES
