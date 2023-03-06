836bed596e1364607ef6cc9f903ac0e026b320fa
Jeremy Johnson <modernclassicgamer@gmail.com>
2023-03-05 21:13:04 -0600

    update roles/shell/files/ssh.sh

--- roles/shell/files/ssh.sh
+++ roles/shell/files/ssh.sh
@@ -1,5 +1,5 @@
 # Ansible managed
 
-chmod 700 "$HOME/.ssh"
+chmod 700 "$HOME/.ssh" 2>/dev/null
 find "$HOME/.ssh" -type f -name '*.pub' -or -name 'authorized_keys' -or -name 'known_hosts' -exec chmod 644 {} \;
 find "$HOME/.ssh" -type f -not -name '*.*' -not -name 'known_hosts' -not -name 'authorized_keys' -exec chmod 600 {} \;
