e8d7dfd4128a60d93f6c08d2747fe9c88f8bd2b8
Jeremy Johnson <modernclassicgamer@gmail.com>
2023-04-13 20:21:14 +0000

    add aliases for 'proxy-switcher'

--- roles/shell/files/aliases.sh
+++ roles/shell/files/aliases.sh
@@ -1,3 +1,5 @@
+# ANSIBLE MANAGED FILE - wsl-setup
+
 alias ls='ls --color=auto'
 alias ll='ls -l'
 alias la='ls -A'
@@ -5,3 +7,6 @@ alias l='ls -CF'
 alias grep='grep --color=auto'
 alias c='clear'
 alias statc='stat -c %a'
+alias psw='source $HOME/source/repos.personal/jeremyj563/proxy-switcher/proxy-switcher.sh'
+alias proxy_on='psw $PROXY'
+alias proxy_off='psw'
