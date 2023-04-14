9171c9c5c59a911e8d8dd8005e1a56331c3fe780
Jeremy Johnson <modernclassicgamer@gmail.com>
2023-04-14 18:27:24 +0000

    update aliases.sh

--- roles/shell/files/aliases.sh
+++ roles/shell/files/aliases.sh
@@ -2,9 +2,9 @@
 
 alias reload!='. ~/.bashrc'
 alias ..='cd ..'
-alias ...='cd ../../../'
-alias ....='cd ../../../../'
-alias .....='cd ../../../../'
+alias ...='cd ../../'
+alias .2='cd ../../'
+alias .3='cd ../../../'
 alias .4='cd ../../../../'
 alias .5='cd ../../../../..'
 alias l.='ls -d .* --color=auto'
