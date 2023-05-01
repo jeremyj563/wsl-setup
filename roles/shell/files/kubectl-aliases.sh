d92bf1a2a089d95a1f7fe85b97ba37c17896450a
Jeremy Johnson <jeremy.johnson4@collins.com>
2023-05-01 13:27:53 -0500

    update .kubectl_aliases path

--- roles/shell/files/kubectl-aliases.sh
+++ roles/shell/files/kubectl-aliases.sh
@@ -1,4 +1,4 @@
 # ANSIBLE MANAGED FILE - wsl-setup
 
-KUBECTL_ALIASES="$HOME/source/repos/jeremyj563/kubectl-aliases/.kubectl_aliases"
+KUBECTL_ALIASES="$HOME/source/repos.personal/jeremyj563/kubectl-aliases/.kubectl_aliases"
 [ -f $KUBECTL_ALIASES ] && source $KUBECTL_ALIASES
