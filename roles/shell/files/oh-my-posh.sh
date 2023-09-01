2e59e452e5563a49137c96663218d81fd9a445d4
Jeremy Johnson <jeremy.johnson4@collins.com>
2023-09-01 14:27:55 -0500

    use omp theme 'jj'

--- roles/shell/files/oh-my-posh.sh
+++ roles/shell/files/oh-my-posh.sh
@@ -1,3 +1,3 @@
 # ANSIBLE MANAGED FILE - wsl-setup
 
-eval "$(oh-my-posh init bash)"
+eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/jeremyj563/wsl-setup/master/files/oh-my-posh/themes/jj.omp.json')"
