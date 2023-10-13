c1903672d38e34b52a6f7b9c775df93915dccfdf
Jeremy Johnson <jeremy.johnson4@collins.com>
2023-10-13 16:25:39 -0500

    update oh-my-posh.sh

--- roles/shell/files/oh-my-posh.sh
+++ roles/shell/files/oh-my-posh.sh
@@ -1,3 +1,10 @@
 # ANSIBLE MANAGED FILE - wsl-setup
 
 eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/jeremyj563/wsl-setup/master/files/oh-my-posh/themes/jj.omp.json')"
+
+alias omp='oh-my-posh'
+function p() {
+    for segment in command kubectl os path time; do
+        oh-my-posh toggle $segment
+    done
+}
