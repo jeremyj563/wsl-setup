5d70f3bbcba7bf3a25cf7a1b2d2ea6ffc84a0293
Jeremy Johnson <jeremy.johnson4@collins.com>
2024-06-01 10:32:26 -0500

    update ssh.sh in role 'shell'

--- roles/shell/files/ssh.sh
+++ roles/shell/files/ssh.sh
@@ -1,5 +1,6 @@
 # ANSIBLE MANAGED FILE - wsl-setup
 
 chmod 700 "$HOME/.ssh" 2>/dev/null
-find "$HOME/.ssh" -type f -name '*.pub' -or -name 'authorized_keys' -or -name 'known_hosts' -exec chmod 644 {} \;
+find "$HOME/.ssh" -type f -name '*.pub' -exec chmod 1130 {} \;
+find "$HOME/.ssh" -type f -name 'authorized_keys' -or -name 'known_hosts' -exec chmod 644 {} \;
 find "$HOME/.ssh" -type f -not -name '*.*' -not -name 'known_hosts' -not -name 'authorized_keys' -exec chmod 600 {} \;
