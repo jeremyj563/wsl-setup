60ba5f3eaab6cd40ee85fc826de6ce54766668ce
Jeremy Johnson <jeremy.johnson4@collins.com>
2024-05-31 21:30:22 -0500

    Revert "update install-ansible.sh"
    This reverts commit 8e28fe6ad1f182c76b614fc38e33762877f69a4c.

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -2,6 +2,6 @@
 
 sudo apt update &&
     sudo apt install -y software-properties-common &&
-    sudo add-apt-repository --yes --update ppa:ansible/ansible &&
+    yes | sudo -E apt-add-repository ppa:ansible/ansible &&
     sudo apt update &&
     sudo apt install -y ansible
